f1e16f0a77c44ac6ef8b571a441e9d76380150bd
Christopher Lee <clee@keplergrp.com>
2021-08-19 16:13:27 -0400

    added example flask application with working postgres db

--- flask_practice_2/flask_practice_2/app.py
+++ flask_practice_2/flask_practice_2/app.py
@@ -1,22 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://chris@localhost/test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
-    body = db.Column(db.String(128))
-
-
-
-"""
-first = Post(name='first post', body='this is the body of my first post')
-db.session.add(first)
-db.session.commit()
-"""
+    name = db.Column(db.String(80))
+    body = db.Column(db.String(120))
 
--- postgres-demo-docker/postgres_demo_docker/app.py
+++ postgres-demo-docker/postgres_demo_docker/app.py
@@ -1,19 +1,27 @@
+import os
 from flask import Flask
-from flask_sqlalchemy import SQLalchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://test:test@dbtest/rando'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://test:test@localhost/rando'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-class Post(db.model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
     body = db.Column(db.String(128))
 
-class User(db.model):
+
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128))
 
-app.run(host='0.0.0.0')
+db.create_all()
+first = Post(name='bobby',body='HEY ITSA ME BOBBY')
+db.session.add(first)
+db.session.commit()
