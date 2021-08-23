74934a48303e00093ba65fdebfa26e018665383a
Christopher Lee <clee@keplergrp.com>
2021-08-23 16:09:02 -0400

    modified files to listen to flask with volume on docker

--- postgres-demo-docker/postgres_demo_docker/app.py
+++ postgres-demo-docker/postgres_demo_docker/app.py
@@ -3,8 +3,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+uri = os.environ["SQLALCHEMY_DATABASE_URI"]
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://test:test@localhost/rando'
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -21,7 +23,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128))
 
-db.create_all()
-first = Post(name='bobby',body='HEY ITSA ME BOBBY')
-db.session.add(first)
-db.session.commit()
+@app.route('/')
+def greeting():
+    return 'hello WHAT UP '
+
+
+app.run(host="0.0.0.0")
