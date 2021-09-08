358489299c53933f13ee574046cff42a99bc1d3e
Christopher Lee <clee@keplergrp.com>
2021-09-08 18:18:40 -0400

    refactored config file

---
+++ book_bank/frontend/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path")
+const Configuration = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const webpack = require("webpack")
+
+module.exports = {
+  mode: "development",
+  output: {
+    publicPath: "/",
+  },
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+   new webpack.HotModuleReplacementPlugin({})
+  ],
+  devtool: "inline-source-map",
+};
+
+
